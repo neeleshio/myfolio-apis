aaeb83f9482c13afc07cf46ece17a629c1423231
Neelesh Shetty <neeleshshetty01@gmail.com>
2021-11-06 23:08:54 +0530

    date formate change

--- src/models/articles.js
+++ src/models/articles.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const articlesSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
-    created: { type: Date, default: Date.now },
+    created: { type: Date, default: Date },
     difficulty: { type: String, required: true },
     topic: { type: String, required: true },
     title: { type: String, required: true },
--- src/models/userAgent.js
+++ src/models/userAgent.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const userAgentSchema = mongoose.Schema({
     _id: mongoose.Types.ObjectId,
-    created: { type: Date, default: Date.now },
+    created: { type: String, default: Date },
     ip: { type: String, required: true },
     os: { type: Object, required: true },
     browser: { type: Object, required: true },
